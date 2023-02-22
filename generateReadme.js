2145fff6ccffead8c41cd82fc9ece595a1b59e76
wilguille <willyguille97@gmail.com>
2023-02-22 20:16:55 +0000

    Created new js file to generate the readme file content, and exported it to use it in the index.js

---
+++ generateReadme.js
@@ -0,0 +1,54 @@
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  
+  To install necessary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/${data.license})
+  
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  
+  ${data.contributing}
+  
+  ## Tests
+  
+  To run tests, run the following command:
+  
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+  
+  ## Questions
+  
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+  `;
+}
+
+module.exports = generateReadme;
--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: "What is your project's title?",
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a brief description of your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'Please provide installation instructions for your project:',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please provide usage instructions for your project:',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'Choose a license for your project:',
-      choices: ['MIT', 'Apache', 'GPL'],
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'Please provide contribution guidelines for your project:',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Please provide instructions for running tests on your project:',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'What is your GitHub username?',
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: 'What is your email address?',
-    },
+{
+    type: 'input',
+    name: 'title',
+    message: "What is your project's title?",
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a brief description of your project:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Please provide installation instructions for your project:',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Please provide usage instructions for your project:',
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache', 'GPL'],
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'Please provide contribution guidelines for your project:',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Please provide instructions for running tests on your project:',
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+    validate: function (input) {
+        // check if email is valid
+        return /\S+@\S+\.\S+/.test(input) ? true : 'Please enter a valid email address';
+      }
+},
