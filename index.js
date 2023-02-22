2f4e969460e7f507678b0d7973af6502f6a1b1ad
wilguille <willyguille97@gmail.com>
2023-02-22 19:59:28 +0000

    Added inquirer prompt method to take user answers about the readme

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is your project's title?",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a brief description of your project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache', 'GPL'],
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?',
+    },
